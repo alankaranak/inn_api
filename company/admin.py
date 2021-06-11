67cccbcc73d1b1ab8eac0b26d17dd311f696e362
ysur67 <rus.muratow@gmail.com>
2021-06-11 21:26:48 +0300

    fixed username

--- company/admin.py
+++ company/admin.py
@@ -63,7 +63,7 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'is_superuser')
+    list_display = ('username', 'is_superuser')
     list_filter = ('is_superuser',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
